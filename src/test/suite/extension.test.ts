bdc9a6e51bfa3b2b49da43c3f3b3bad48f75caa9
jdhameli <107123702+jdhameli@users.noreply.github.com>
2022-12-20 15:32:22 -0500

    fix test

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -10,6 +10,6 @@ suite('Extension Test Suite', () => {
 
 	test('Sample test', () => {
 		assert.strictEqual([1, 2, 3].indexOf(5), -1);
-		assert.strictEqual([1, 2, 3].indexOf(2), -1);
+		assert.strictEqual([1, 2, 3].indexOf(0), -1);
 	});
 });
